7cbf0a4add75f43d1eecaabcd936ef4859a3a18d
Pedro Costa <dev@iampfac.com>
2016-11-28 12:55:19 +0000

    Automated commit at 2016-11-28 12:55:19 UTC by middleman-deploy 2.0.1-alpha

---
+++ javascripts/toggle.js
@@ -0,0 +1,1 @@
+!function(t){t(document).ready(function(){var a=(t(".Burger"),t("[data-toggle]"));a.click(function(a){var e=t(a.currentTarget),r=e.data("toggle"),g=e.data("target"),n=t(g);n.toggleClass(r)})})}(jQuery);
